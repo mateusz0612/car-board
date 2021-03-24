1cfcc9d77cab99cc72cbab880d9e0fc264644473
Alan S <szuszkiewiczalan928@gmail.com>
2021-03-24 18:17:35 +0100

    Created superuser, created basic model skeleton
    Modification of settings to add superuser, as well as rest_framework and api to our settings.py

--- carboard/api/models.py
+++ carboard/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# before sending data to clients, we need to serialize it - JSON as endpoint
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    email = models.EmailField(max_length=75)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
