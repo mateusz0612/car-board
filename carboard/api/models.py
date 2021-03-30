431cb9debbfc6fc4968251d40cfcd6db996254cc
Alan S <szuszkiewiczalan928@gmail.com>
2021-03-30 23:36:09 +0200

    Modified model information in preparation for view

--- carboard/api/models.py
+++ carboard/api/models.py
@@ -9,5 +9,20 @@ class Article(models.Model):
     email = models.EmailField(max_length=75)
     date = models.DateTimeField(auto_now_add=True)
 
+class Details(models.Model):
+    modelList = [
+        ("Abarth", "Abarth"), ("Alfa Romeo", "Alfa Romeo"), ("Aston Martin", "Aston Martin"), ("Audi", "Audi"),
+        ("Bentley", "Bentley"),
+        ("BMW", "BMW"), ("Bugatti", "Bugatti"), ("Cadillac", "Cadillac"), ("Chevrolet", "Chevrolet"),
+        ("Other", "Other"),  # other option for exceptions to list
+        ("Citroen", "Citroen"), ("Dacia", "Dacia"), ("Daewoo", "Daewoo")
+    ]
+    carMake = models.CharField(max_length=50, choices=modelList)
+    carModel = models.CharField(max_length=50)  # draw from our predefined model list
+    carPaint = models.CharField(max_length=50)
+    carVin = models.CharField(max_length=17)  # Always 17 characters max
+    carMileage = models.IntegerField()
+
+
     def __str__(self):
         return self.title
